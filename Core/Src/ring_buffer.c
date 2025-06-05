cde698c27ddc363d96eff7d36087b0957f6a7dd8
sgomezmunoz <sgomezmu@unal.edu.co>
2025-06-05 12:46:41 -0500

    adelanto se crearon las carpetas

---
+++ Core/Src/ring_buffer.c
@@ -0,0 +1,38 @@
+#include "ring_buffer.h"
+
+void ring_buffer_init(ring_buffer_t *rb, uint8_t *buffer, uint16_t capacity) 
+{
+
+}
+
+bool ring_buffer_write(ring_buffer_t *rb, uint8_t data)
+{
+
+    return true;
+}
+
+bool ring_buffer_read(ring_buffer_t *rb, uint8_t *data)
+{
+
+    return true;
+}
+
+uint16_t ring_buffer_count(ring_buffer_t *rb)
+{
+
+}
+
+bool ring_buffer_is_empty(ring_buffer_t *rb)
+{
+
+}
+
+bool ring_buffer_is_full(ring_buffer_t *rb)
+{
+
+}
+
+void ring_buffer_flush(ring_buffer_t *rb)
+{
+
+}
